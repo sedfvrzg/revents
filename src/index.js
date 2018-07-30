04cecc323f59e74a483cad25563660304cf7c75d
DESKTOP-1UFA0VS\Peter <newtobby@hotmail.com>
2018-07-30 18:08:29 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app/layout/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootElement = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render( <App />, rootElement )
+}
+
+if (module.hot) {
+    module.hot.accept('./app/layout/App', () => {
+        setTimeout(render);
+    })
+}
+
+render();
+
+ReactDOM.render(<App />, rootElement);
+registerServiceWorker();
